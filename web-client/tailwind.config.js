083d0b91800601009e119c087a2445a85b0fc64c
Radek Tomasek <radek.tomasek@gmail.com>
2024-05-31 22:28:38 +0100

    (feat): Extend the Tailwind configuration and add the first component integrating with Storybook (#2)
    * This commit contains the following changes:
    
    - Add a button component based on the Figma design
    - Preset the most common stuff related to the design system
    - Fixed the test settings and wrote a simple test for the Button component
    - Made the Button component universal, ready to accept the standard parameters
    
    * Updated the changelog

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -4,7 +4,66 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}", "index.html"],
   prefix: "",
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xs": [
+          "0.75rem",
+          {
+            lineHeight: 1.5,
+            fontWeight: 700,
+          },
+        ],
+        xs: [
+          "0.8125rem",
+          {
+            lineHeight: 1.77,
+            fontWeight: 500,
+          },
+        ],
+        base: [
+          "0.9375rem",
+          {
+            lineHeight: 1.5,
+            fontWeight: 700,
+          },
+        ],
+        lg: [
+          "1.125rem",
+          {
+            lineHeight: 1.5,
+            fontWeight: 700,
+          },
+        ],
+        xl: [
+          "1.5rem",
+          {
+            lineHeight: 1.5,
+            fontWeight: 700,
+          },
+        ],
+      },
+      colors: {
+        custom: {
+          "dark-purple": "hsl(242, 48%, 58%)",
+          "light-purple": "hsl(243, 100%, 82%)",
+          "light-purple-10": "hsla(242, 48%, 58%, 0.1)",
+          "light-purple-25": "hsla(242, 48%, 58%, 0.25)",
+          black: "hsl(237, 100%, 4%)",
+          "very-dark-grey": "hsl(235, 16%, 15%)",
+          "dark-grey": "hsl(235, 12%, 19%)",
+          "dark-lines": "hsl(236, 11%, 27%)",
+          "medium-grey": "hsl(216, 15%, 57%)",
+          "medium-lines": "hsl(221, 69%, 94%)",
+          "light-grey": "hsl(220, 69%, 97%)",
+          white: "hsl(0, 0%, 100%)",
+          red: "hsl(0, 78%, 63%)",
+          "light-red": "hsl(0, 100%, 80%)",
+        },
+      },
+      fontFamily: {
+        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
