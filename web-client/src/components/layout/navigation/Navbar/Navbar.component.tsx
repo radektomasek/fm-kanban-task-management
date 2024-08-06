e84b9f0084c7bf52d31d08ff4aeecae13692582b
Radek Tomasek <radek.tomasek@gmail.com>
2024-08-06 22:45:24 +0100

    Finished the modal control - having an option to click outside of the modal + having a better control via keyboard.

--- web-client/src/components/layout/navigation/Navbar/Navbar.component.tsx
+++ web-client/src/components/layout/navigation/Navbar/Navbar.component.tsx
@@ -32,7 +32,7 @@ export const Navbar = ({ testId, boardName }: Props) => {
         <ConfirmationDialog
           title="Delete this board?"
           description="Are you sure you want to delete the 'Platform Launch' board? This action will remove all columns and tasks and cannot be reversed."
-          onDelete={() => {}}
+          onDelete={() => setShowModal(false)}
           onCancel={() => setShowModal(false)}
         />
       </Modal>
