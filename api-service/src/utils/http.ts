b914ae8aa0d409241f1972652fb38541af5fbbec
Radek Tomasek <radek.tomasek@gmail.com>
2025-01-04 23:25:44 -0800

    Added basic utility functions

---
+++ api-service/src/utils/http.ts
@@ -0,0 +1,33 @@
+import { FastifyReply } from "fastify"
+import type { StatusCodes } from "http-status-codes"
+import { z } from "zod"
+
+type HttpError = {
+  reply: FastifyReply
+  message: string;
+  code: StatusCodes;
+  cause?: string
+}
+
+export function httpError({
+  reply,
+  message,
+  code,
+  cause,
+}: HttpError) {
+  return reply.status(code).send({
+    message, cause
+  })
+}
+
+export const httpErrorSchema = z.object({
+  message: z.string(),
+  cause: z.string().optional()
+})
+
+export const errorResponses = {
+  400: httpErrorSchema,
+  401: httpErrorSchema,
+  404: httpErrorSchema,
+  500: httpErrorSchema
+}
