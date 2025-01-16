b812ebdf628002d671229a71c109619962aac7cf
Radek Tomasek <radek.tomasek@gmail.com>
2025-01-15 20:23:52 -0800

    Replaced column model for creating and updating data and started writing function for creating columns

--- api-service/src/modules/column/column.schema.ts
+++ api-service/src/modules/column/column.schema.ts
@@ -1,36 +1,17 @@
 import { z } from "zod"
-import { createSelectSchema, createUpdateSchema } from "drizzle-zod"
-import { columns, projects } from "../../db/schema"
-import { errorResponses } from "../../utils/http"
 
-export const createColumnSchema = {
-  tags: ["columns"],
-  params: z.object({
+export const createColumnSchema = z.array(
+  z.object({
     projectId: z.string(),
     boardId: z.string(),
-  }),
-  body: z.object({
     name: z.string(),
-  }),
-  response: {
-    201: createSelectSchema(columns),
-    ...errorResponses,
-  },
-} as const
+  })
+)
 
-export const updateColumnSchema = {
-  tags: ["columns"],
-  params: z.object({
+export const updateColumnSchema = z.array(
+  z.object({
     projectId: z.string(),
     boardId: z.string(),
-  }),
-  body: createUpdateSchema(columns).omit({
-    id: true,
-    createdAt: true,
-    updatedAt: true,
-  }),
-  response: {
-    200: createSelectSchema(projects),
-    ...errorResponses,
-  } as const,
-}
+    columnId: z.string().optional(),
+  })
+)
--- api-service/src/modules/column/column.service.ts
+++ api-service/src/modules/column/column.service.ts
@@ -1,11 +1,26 @@
-import { DB } from "../../db"
+import { z } from "zod"
 import { eq } from "drizzle-orm"
 import { columns } from "../../db/schema"
 import { logger } from "../../utils/logger"
+import { createColumnSchema, updateColumnSchema } from "./column.schema"
+import { DB } from "../../db"
 
-export async function createColumns() {}
+export async function createColumns(
+  input: z.infer<typeof createColumnSchema>,
+  db: DB
+) {
+  /**
+   * @TODO: need to add color function
+   */
+  const result = await db.insert(columns).values(input).returning()
 
-export async function updateColumns() {}
+  return result
+}
+
+export async function updateColumns(
+  input: z.infer<typeof updateColumnSchema>,
+  db: DB
+) {}
 
 export async function getColumnsByBoardId(boardId: string, db: DB) {
   try {
