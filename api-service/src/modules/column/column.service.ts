0be4e640cbdb308f7b6b57679d0aecb5338ee163
Radek Tomasek <radek.tomasek@gmail.com>
2025-01-18 07:48:08 -0800

    Added updateColumns function implementation and wrapped the db logic by try/catch within column.service

--- api-service/src/modules/column/column.service.ts
+++ api-service/src/modules/column/column.service.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { eq } from "drizzle-orm"
+import { eq, sql } from "drizzle-orm"
 import { columns } from "../../db/schema"
 import { logger } from "../../utils/logger"
 import { createColumnSchema, updateColumnSchema } from "./column.schema"
@@ -10,23 +10,53 @@ export async function createColumns(
   input: z.infer<typeof createColumnSchema>,
   db: DB
 ) {
-  const result = await db
-    .insert(columns)
-    .values(
-      input.map((element) => ({
-        ...element,
-        color: generateStatusColor(),
-      }))
-    )
-    .returning()
+  try {
+    const result = await db
+      .insert(columns)
+      .values(
+        input.map((element) => ({
+          ...element,
+          color: generateStatusColor(),
+        }))
+      )
+      .returning()
 
-  return result
+    return result
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown error"
+    logger.error({ message }, "updateColumns: failed to insert data")
+  }
 }
 
 export async function updateColumns(
   input: z.infer<typeof updateColumnSchema>,
   db: DB
-) {}
+) {
+  try {
+    const result = await db
+      .insert(columns)
+      .values(
+        input.map((element) => ({
+          id: element.columnId,
+          name: element.name,
+          projectId: element.projectId,
+          boardId: element.boardId,
+          color: generateStatusColor(),
+        }))
+      )
+      .onConflictDoUpdate({
+        target: columns.id,
+        set: {
+          name: sql.raw(`excluded.${columns.name.name}`),
+        },
+      })
+      .returning()
+    return result
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown error"
+    logger.error({ message }, "updateColumns: failed to update data")
+  }
+}
 
 export async function getColumnsByBoardId(boardId: string, db: DB) {
   try {
